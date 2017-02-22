5caee3c31a6166a38891db9670258e9c6dbdf9f5
Netanel Sananes <netanelile@gmail.com>
2017-02-22 14:47:16 +0200

    bug fix on preference activity

--- app/src/main/java/com/example/netan/myapp/Preferences.java
+++ app/src/main/java/com/example/netan/myapp/Preferences.java
@@ -9,15 +9,15 @@ import android.preference.PreferenceFragment;
 public class Preferences extends PreferenceActivity{
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferencesFragment()).commit();
     }
 
     public static class MyPreferencesFragment extends PreferenceFragment{
 
         @Override
-        public void onCreate(Bundle savedInstanceState) {
+        public void onCreate(final Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.prefs);
 
